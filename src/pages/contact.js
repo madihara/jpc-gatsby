67d39f0f749e073b52c5924681ed43330a21207b
Madison Haradine <madisonharadine@gmail.com>
2019-11-14 16:24:28 -0500

    added hero to contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,27 @@
 import React from "react"
 import Layout from "../components/Layout"
+import LazyHero from "react-lazy-hero"
+import HeroHeader from "../components/HeroHeader"
+import Zoom from "react-reveal"
 import ContactForm from "../components/ContactForm"
+
+import heroimg from "../images/jezael-melgoza-HYQvV8wWX18-unsplash.jpg"
 
 const contact = () => (
   <Layout>
+    <LazyHero
+      imageSrc={heroimg}
+      opacity="0.5"
+      color="#0a1128"
+      parallaxOffset="1"
+      minHeight="45vh"
+      isCentered={true}
+      className="hero-container"
+    >
+      <Zoom>
+        <HeroHeader title="Contact Us"></HeroHeader>
+      </Zoom>
+    </LazyHero>
     <ContactForm />
   </Layout>
 )

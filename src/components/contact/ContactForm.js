10c131724a19b6d0a02fe94a9d72ec65169b7f90
Madison Haradine <madisonharadine@gmail.com>
2020-02-07 16:47:00 -0500

    lots of commits because i want netlify to update

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -81,7 +81,7 @@ const ContactForm = () => {
           <iframe
             title="map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7609.203895273583!2d-86.02549457468723!3d41.72893430399752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8816db791dd64e67%3A0x25f3651a67b5576!2s2926%20Paul%20Dr%2C%20Elkhart%2C%20IN%2046514!5e0!3m2!1sen!2sus!4v1573413624243!5m2!1sen!2sus"
-            width="400"
+            width="380"
             height="300"
             frameborder="0"
           ></iframe>

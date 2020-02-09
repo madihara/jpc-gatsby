85c502377db7088be6a626f051a2e6c6e5f97029
Madison Haradine <madisonharadine@gmail.com>
2020-02-09 10:39:54 -0500

    capabilities tabs created

--- src/pages/capabilities.js
+++ src/pages/capabilities.js
@@ -4,8 +4,12 @@ import Layout from "../components/Layout"
 import LazyHero from "react-lazy-hero"
 import HeroHeader from "../components/hero/HeroHeader"
 import Zoom from "react-reveal"
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs"
 
 import heroimg from "../images/jezael-melgoza-HYQvV8wWX18-unsplash.jpg"
+
+import "react-tabs/style/react-tabs.css"
+import styles from "./styles-pages/capabilities.module.css"
 
 const capabilities = () => {
   return (
@@ -22,15 +26,29 @@ const capabilities = () => {
           <HeroHeader title="manufacturing capabilities" />
         </Zoom>
       </LazyHero>
-      <div>
-        <h1> Many Capabilties</h1>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-        </ul>
+      <div className={styles.container}>
+        <Tabs defaultIndex={0} onSelect={index => console.log(index)}>
+          <TabList>
+            <Tab>Single Lock Stitching</Tab>
+            <Tab>Double Lock Stitching</Tab>
+            <Tab>Long Arm Sewing</Tab>
+            <Tab>Double Axis Cutting Table</Tab>
+            <Tab>Bar Tackers</Tab>
+            <Tab>Grommetting</Tab>
+          </TabList>
+          <TabPanel className={styles.panel}>
+            <ul>
+              <li>thing 1</li>
+              <li> thing 2</li>
+              <li> thing 3</li>
+            </ul>
+          </TabPanel>
+          <TabPanel className={styles.panel2}>Goodbye</TabPanel>
+          <TabPanel className={styles.panel2}>3</TabPanel>
+          <TabPanel className={styles.panel2}>4</TabPanel>
+          <TabPanel className={styles.panel2}>5</TabPanel>
+          <TabPanel className={styles.panel2}>6</TabPanel>
+        </Tabs>
       </div>
     </Layout>
   )

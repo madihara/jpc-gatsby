e383c58a0e1cc915ec44845b52a41bc109546db4
Madison Haradine <madisonharadine@gmail.com>
2020-03-14 09:58:36 -0400

    minor tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,12 +32,5 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-      }
-    }
   ],
 }
